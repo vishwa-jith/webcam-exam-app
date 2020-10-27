c5fb63adb251151e45f1e1ead2accadbef480c2f
vishwa-jith <vishwajith567@gmail.com>
2020-10-27 12:08:55 +0530

    Change import Name

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Consultant from "./pages/Test";
+import Test from "./pages/Test";
 import Settings from "./pages/Settings";
 import Help from "./pages/Help";
 import Home from "./pages/LandingPage";
 const Routes = () => {
   return (
     <Switch>
-      <Route component={Consultant} path="/test" exact />
+      <Route component={Test} path="/test" exact />
       <Route component={Settings} path="/settings/home" exact />
       <Route component={Settings} path="/settings/personal" exact />
       <Route component={Settings} path="/settings/company" exact />
