3971e982d31c28a7194e96c932de835c7c828d12
vishwa-jith <vishwajith567@gmail.com>
2020-10-28 19:10:35 +0530

    Create Test Questions UI skeleton

--- src/pages/Test/test.container.jsx
+++ src/pages/Test/test.container.jsx
@@ -7,6 +7,14 @@ const Test = () => {
   const [captureTimer, setCaptureTimer] = useState(null);
   const menu = [{ label: "Previous" }, { label: "Next" }];
   const webcamRef = useRef(null);
+  const [answers, setAnswers] = useState([]);
+  const [question_no, setQuestion_No] = useState(0);
+  const questions = [
+    { topic: "sample question", options: ["opt1", "opt2", "opt3", "opt4"] },
+    { topic: "sample question", options: ["opt1", "opt2", "opt3", "opt4"] },
+    { topic: "sample question", options: ["opt1", "opt2", "opt3", "opt4"] },
+    { topic: "sample question", options: ["opt1", "opt2", "opt3", "opt4"] },
+  ];
   const takeScreenShoot = useCallback(() => {
     setCaptureTimer(
       setInterval(() => {
@@ -20,6 +28,29 @@ const Test = () => {
   //     clearInterval(captureTimer);
   //   };
   // });
+  const handleAnswers = (event) => {
+    console.log(answers);
+    if (answers.length >= question_no + 1) {
+      setAnswers(
+        answers.map((opt, qno) => {
+          if (qno === question_no) {
+            console.log(event.target.id, question_no);
+            return parseInt([event.target.id]);
+          }
+          return opt;
+        })
+      );
+    } else {
+      setAnswers([...answers, parseInt([event.target.id])]);
+    }
+  };
+  const handleQuestion = (type) => {
+    if (type < 0) {
+      setQuestion_No(question_no - 1);
+    } else {
+      setQuestion_No(question_no + 1);
+    }
+  };
   useEffect(() => {
     document.addEventListener("visibilitychange", function () {
       document.title = document.visibilityState;
@@ -34,6 +65,11 @@ const Test = () => {
         webcamRef={webcamRef}
         takeScreenShoot={takeScreenShoot}
         src={src}
+        questions={questions}
+        question_no={question_no}
+        handleAnswers={handleAnswers}
+        answers={answers}
+        handleQuestion={handleQuestion}
       />
     </>
   );
--- src/pages/Test/test.view.jsx
+++ src/pages/Test/test.view.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import Webcam from "react-webcam";
-import { Grid, Paper, Box } from "@material-ui/core";
+import {
+  Grid,
+  Paper,
+  Box,
+  Typography,
+  FormControlLabel,
+  Checkbox,
+  List,
+  ListItem,
+  Button,
+} from "@material-ui/core";
 const videoConstraints = {
   width: 1280,
   height: 720,
   facingMode: "user",
 };
-const TestView = ({ webcamRef, takeScreenShoot, src }) => {
+const TestView = ({
+  webcamRef,
+  takeScreenShoot,
+  src,
+  questions,
+  question_no,
+  handleAnswers,
+  answers,
+  handleQuestion,
+}) => {
   return (
     <>
       <Grid
@@ -31,9 +50,76 @@ const TestView = ({ webcamRef, takeScreenShoot, src }) => {
           </Paper>
         </Grid>
       </Grid>
-      {src !== null && (
+      {/* {src !== null && (
         <img src={src} alt="sample" width="100px" height="100px" />
-      )}
+      )} */}
+      <Grid container justify="center" alignItems="center" xs={12}>
+        <Grid item md={10} xs={12}>
+          <Box my={4}>
+            <Paper>
+              <Typography variant="h6">
+                {questions[question_no].topic}
+              </Typography>
+              <List dense>
+                {questions[question_no].options.map((opt, index) => {
+                  return (
+                    <ListItem button id={`${index}`} onClick={handleAnswers}>
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            checked={
+                              answers.length !== question_no &&
+                              answers[question_no] === index
+                            }
+                            onChange={handleAnswers}
+                            name={`${index}`}
+                            id={`${index}`}
+                            color="primary"
+                          />
+                        }
+                        label={opt}
+                      />
+                    </ListItem>
+                  );
+                })}
+              </List>
+              <Grid
+                container
+                justify="space-around"
+                alignItems="center"
+                xs={12}
+              >
+                <Grid item>
+                  {question_no === 0 ? (
+                    <Button disabled={true}>Start</Button>
+                  ) : (
+                    <Button
+                      onClick={() => {
+                        handleQuestion(-1);
+                      }}
+                    >
+                      Previous
+                    </Button>
+                  )}
+                </Grid>
+                <Grid item>
+                  {question_no === questions.length - 1 ? (
+                    <Button>Submit</Button>
+                  ) : (
+                    <Button
+                      onClick={() => {
+                        handleQuestion(1);
+                      }}
+                    >
+                      Next
+                    </Button>
+                  )}
+                </Grid>
+              </Grid>
+            </Paper>
+          </Box>
+        </Grid>
+      </Grid>
     </>
   );
 };
