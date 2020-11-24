6d1e044f06cf5e4bcbd6e1cc96160ec134789e24
vishwa-jith <vishwajith567@gmail.com>
2020-11-24 22:44:58 +0530

    Modify Import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "redux";
+import { Provider } from "react-redux";
 import { configureReducer } from "./redux/config.reducer";
 function App() {
   const store = configureReducer();
