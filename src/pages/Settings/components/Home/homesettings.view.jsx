06bb03f09701eacab799e41571e7b26be2c6030d
vishwa-jith <vishwajith567@gmail.com>
2020-12-07 14:59:13 +0530

    Modify Settings UI

--- src/pages/Settings/components/Home/homesettings.view.jsx
+++ src/pages/Settings/components/Home/homesettings.view.jsx
@@ -11,12 +11,13 @@ import {
   Box,
   Paper,
   useMediaQuery,
+  Avatar,
 } from "@material-ui/core";
 import EditIcon from "@material-ui/icons/Edit";
 import SaveIcon from "@material-ui/icons/Save";
+import PersonIcon from "@material-ui/icons/Person";
 import { useTheme } from "@material-ui/core/styles";
-import { baseUrl } from "../../../../components/constants";
-
+import { grey } from "@material-ui/core/colors";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -32,6 +33,10 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: "#90caf9",
     color: "white",
   },
+  badge: {
+    width: theme.breakpoints.up("sm") ? "200px" : "100px",
+    height: theme.breakpoints.up("sm") ? "200px" : "100px",
+  },
 }));
 const HomeSettingsView = ({
   isEdit,
@@ -43,81 +48,70 @@ const HomeSettingsView = ({
   const classes = useStyles();
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.up("sm"));
+
   return (
     <>
       <form noValidate autoComplete="off">
-        <Grid container flexDirection="column">
-          <Grid item container justify="center" alignItems="center" sm={5}>
-            <Box mx={3} mb={3}>
-              <Paper elevation={3}>
-                <img
-                  src={`${baseUrl}images/profile.jpg`}
-                  width="100%"
-                  style={{ height: matches ? "75vh" : "40vh" }}
-                  alt="Profile"
-                />
-              </Paper>
-            </Box>
-          </Grid>
-          <Grid item container justify="center" alignItems="flex-start" sm={7}>
-            <Grid item container xs={12}>
-              <Paper style={{ width: "100%" }}>
-                <List>
-                  <ListItem>
-                    <ListItemText>
-                      <Typography
-                        variant="h3"
-                        id="username"
-                        className={classes.username}
-                      >
-                        {userDetails && userDetails.username}
-                      </Typography>
-                    </ListItemText>
-                  </ListItem>
-                  <ListItem>
-                    <>
-                      <Box>
-                        <Grid container flexDirection="column">
-                          <TextField
-                            size="small"
-                            value={names.firstname}
-                            variant={matches && !isEdit ? "standard" : "filled"}
-                            label="First Name"
-                            margin="dense"
-                            id="firstname"
-                            name="firstname"
-                            inputProps={{ readOnly: isEdit ? false : true }}
-                            onChange={handleChange}
-                          />
-
-                          <TextField
-                            size="small"
-                            value={names.lastname}
-                            variant={matches && !isEdit ? "standard" : "filled"}
-                            label="Last Name"
-                            margin="dense"
-                            id="lastname"
-                            name="lastname"
-                            inputProps={{ readOnly: isEdit ? false : true }}
-                            onChange={handleChange}
-                          />
-                        </Grid>
-                      </Box>
-                    </>
-                    <ListItemSecondaryAction>
-                      <IconButton
-                        onClick={handleOperation}
-                        style={{
-                          backgroundColor: isEdit ? "#4caf50" : "#90caf9",
-                          color: "white",
-                        }}
-                      >
-                        {isEdit ? <SaveIcon /> : <EditIcon />}
-                      </IconButton>
-                    </ListItemSecondaryAction>
-                  </ListItem>
-                </List>
-              </Paper>
+        <Grid container flexDirection="column" component={Paper}>
+          <Grid item container justify="center" alignItems="center">
+            <Grid item container xs={12} justify="center">
+              <Grid
+                container
+                item
+                xs={12}
+                justify="center"
+                style={{ height: "200px", backgroundColor: grey[200] }}
+              >
+                <Avatar className={classes.badge} style={{ marginTop: "75px" }}>
+                  <PersonIcon style={{ fontSize: "200px" }} />
+                </Avatar>
+              </Grid>
+              <Grid item container xs={12} justify="flex-end">
+                <IconButton
+                  onClick={handleOperation}
+                  style={{
+                    backgroundColor: isEdit ? "#4caf50" : "#90caf9",
+                    color: "white",
+                  }}
+                >
+                  {isEdit ? <SaveIcon /> : <EditIcon />}
+                </IconButton>
+              </Grid>
+              <Grid item container xs={12} component={Box} p={2}>
+                <>
+                  <Grid item xs={12}>
+                    <Typography className={classes.username}>
+                      {userDetails && userDetails.username}
+                    </Typography>
+                    <Typography>
+                      <small>
+                        <strong>
+                          {isEdit ? (
+                            <>
+                              <input
+                                style={{ width: "100px" }}
+                                value={names.firstname}
+                                id="firstname"
+                                name="firstname"
+                                onChange={handleChange}
+                              />
+                              <input
+                                style={{ width: "50px" }}
+                                value={names.lastname}
+                                id="lastname"
+                                name="lastname"
+                                onChange={handleChange}
+                              />
+                            </>
+                          ) : (
+                            "# " + names.firstname + " " + names.lastname
+                          )}
+                        </strong>
+                      </small>
+                    </Typography>
+                  </Grid>
+                </>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
